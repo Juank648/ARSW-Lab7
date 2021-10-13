ec6f891b9bca040701e8f61769579b54cb32ed0a
Juan Alayon <jkam86425@gmail.com>
2021-10-13 16:46:55 -0500

    PARTE FINAL

--- src/main/java/edu/eci/arsw/collabpaint/STOMPMessagesHandler.java
+++ src/main/java/edu/eci/arsw/collabpaint/STOMPMessagesHandler.java
@@ -7,8 +7,12 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class STOMPMessagesHandler {
+    public List<Point> listaPuntos = new ArrayList<>();
 
     @Autowired
     SimpMessagingTemplate msgt;
@@ -19,4 +23,21 @@ public class STOMPMessagesHandler {
         System.out.println("Enviado a:"+"/topic/newpoint"+numdibujo);
         msgt.convertAndSend("/topic/newpoint."+numdibujo, pt);
     }
+
+    @MessageMapping("/newpolygon.{numdibujo}")
+    public void handlePolygonEvent(Point pt, @DestinationVariable String numdibujo) throws Exception {
+
+        listaPuntos.add(pt);
+        System.out.println("Cantidad de puntos registrados: "+listaPuntos.size());
+        if(listaPuntos.size() > 2){
+            System.out.println("Lista Completa: ");
+            System.out.println(listaPuntos);
+            System.out.println("Enviado a: "+"/topic/newpolygon."+numdibujo);
+            msgt.convertAndSend("/topic/newpolygon."+numdibujo, listaPuntos);
+            listaPuntos.clear();
+        }
+
+    }
+
+
 }
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -17,6 +17,23 @@ var app = (function () {
         ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
         ctx.stroke();
     };
+
+    var drawPolygon = function (polygon) {
+        console.log("Entrando a draw polygon");
+        console.log(polygon);
+        var canvas = document.getElementById("canvas");
+        var ctx = canvas.getContext("2d");
+        ctx.beginPath();
+        ctx.moveTo(polygon[0].x, polygon[0].y);
+        polygon.forEach(point => {
+            const {x,y} = point;
+            ctx.lineTo(x, y);
+            ctx.stroke();
+        })
+        ctx.closePath();
+        ctx.fill();
+
+    }
     
     
     var getMousePosition = function (evt) {
@@ -40,7 +57,12 @@ var app = (function () {
             stompClient.subscribe('/topic/newpoint.'+idnumber, function (eventbody) {
                 const object = JSON.parse(eventbody.body);
                 addPointToCanvas(object);
-            })
+            });
+            stompClient.subscribe('/topic/newpolygon.'+idnumber, function (eventbody){
+                const polygon = JSON.parse((eventbody.body));
+                drawPolygon(polygon);
+
+            });
         });
 
     };
@@ -56,6 +78,7 @@ var app = (function () {
                 addPointToCanvas(pt);
 
                 stompClient.send("/app/newpoint."+idnumber, {}, JSON.stringify(pt));
+                stompClient.send("/app/newpolygon."+idnumber, {}, JSON.stringify(pt));
             });
         }
     }
@@ -79,6 +102,7 @@ var app = (function () {
             console.info("publishing point at "+pt);
             addPointToCanvas(pt);
             stompClient.send("/app/newpoint."+idnumber, {}, JSON.stringify(pt));
+            stompClient.send("/app/newpolygon."+idnumber, {}, JSON.stringify(pt));
             //publicar el evento
         },
 
